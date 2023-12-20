f4d79c4072ad540cd8cfbc87e64d9836cc2e3ec5
AhmedGhanem50 <ahmedghanemcs@gmail.com>
2023-12-20 02:48:57 +0200

    Your commit message here

--- index.php
+++ index.php
@@ -13,40 +13,60 @@ require_once "includes/addsite_view.inc.php";
     <!-- Link to your external stylesheet -->
     <link rel="stylesheet" href="style.css" />
     <title>Black Box</title>
+    <head>
+    <!-- Other meta tags and stylesheets -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+    <script src="script.js"></script>
+
 </head>
 
 <body>
-    <!-- Header Section -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Your Website</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
     <header>
-        <!-- Logo and Project Name Container -->
-        <div class="logo-container">
-            <a href="index.php">
-                <button class="logo-container">
-                    <div class="header-logo">
-                        <img src="images.png" alt="Your Logo">
-                    </div>
-                    <div class="project-name">Black Box</div>
-                </button>
+        <!-- Navigation Bar -->
+        <nav class="navbar">
+            <a href="index.php" class="logo-button">
+                <img src="images.png" alt="Your Logo" class="navbar-logo">
+                <span class="project-name">Black Box</span>
             </a>
-        </div>
-        <!-- Navigation Buttons -->
-        <!-- Add Site Button -->
-        <a href="#main-section">
-            <button>Add Site</button>
-        </a>
-        <!-- Log In Button -->
-        <!-- About Us Button -->
-        <a href="#aboutUs">
-            <button>About us</button>
-        </a>
-        <a href="login.php">
-            <button>
-                <?php output_username() ?>
-            </button>
-        </a>
-
+            <ul class="nav-links">
+                <li><a href="Favorite.php">Favorite</a></li>
+                <li><a href="#footer">About us</a></li>
+                <li><a href="login.php"> <?php output_username() ?></a></li>
+            </ul>
+        </nav>
     </header>
-    <!-- Main Section -->
+</body>
+</html>
+
+
+<div class="columns-container">
+    <div class="column">
+    <div class="card">
+    <div class="card-body" >
+        <p>Site Name:</p>
+        <p>Username:</p>
+        <p>Email:</p>
+        <p>Password:</p>
+        <div class="icon-container">
+                <i class="fas fa-trash-alt remove-icon"></i>
+                <i class="far fa-star favorite-icon"></i>
+                        </div>
+       
+    </div>
+</div>
+
+
+
+    </div>
+    <div class="column">
     <section id="main-section" class="main-section">
         <!-- Container for Site Details Form and Added Data -->
         <div class="center-container">
@@ -98,59 +118,30 @@ require_once "includes/addsite_view.inc.php";
         </div>
     </section>
 
-    <!-- Footer Section -->
+    </div>
+</div>
 
-    <footer>
-        <div id="aboutUs">
-            <h2>About Us</h2>
-            <!-- About Us Content -->
+  
 
-            <p>Team Introduction: The Web Devolpment <br>
 
-                Welcome to the realm of the Web Devolpment, where creativity meets expertise to drive success! Our team
-                is a
-                diverse group of talented individuals united by a passion for innovation and a commitment to excellence.
-                <br>
-
-                Meet Our Team: <br>
-
-                Khalid - The Visionary Leader: With a keen eye for strategy, John leads our team with a vision for the
-                future.
-                His leadership inspires us to reach new heights. <br>
-
-                Moustafa - The Creative Maven: Sarah's creative genius knows no bounds. From ideation to execution, she
-                brings
-                ideas to life with flair and imagination. <br>
-
-                Anas - The Tech Guru: In the ever-evolving tech landscape, Mike is our go-to expert. His mastery of
-                cutting-edge
-                technologies keeps us ahead of the curve. <br>
-
-                Ahmed- The Organizational Dynamo: Emma ensures the seamless functioning of our team. Her organizational
-                prowess
-                keeps projects on track and deadlines met. <br>
-
-                Mohamed - The Problem Solver: When challenges arise, Alex is the problem-solving maestro. His analytical
-                mind
-                and resourcefulness make seemingly impossible tasks achievable. <br>
-
-                Our Collaborative Spirit: <br>
-
-                What sets us apart is our collaborative spirit. We believe in the power of teamwork, where each member's
-                strengths complement the others. Together, we navigate challenges, celebrate victories, and constantly
-                push the
-                boundaries of what's possible.
-
-                Our Mission: <br>
-
-                The Web Devolpment are on a mission to redefine success through innovation. We strive not only to meet
-                expectations but to exceed them, leaving a lasting impact on every project we undertake.
-            </p>
-        </div>
+<footer id="footer">
+        <h2>About Us</h2>
+        <p>Team Introduction: The Web Development</p>
+        <p>Welcome to the realm of Web Development, where creativity meets expertise to drive success! Our team is a diverse group of talented individuals united by a passion for innovation and a commitment to excellence.</p>
+        
+        <h3>Meet Our Team:</h3>
+        <ul>
+            <li class="team-member">Khalid - Visionary Leader and Backend Developer: A leader in both the ICPC community and as a backend developer. Khalid's problem-solving skills and dedication reflect in his Computer Science studies, where he's at the third level.</li>
+            <li class="team-member">Ahmed - Frontend Developer and Community Vice-Leader: Ahmed contributes as a skilled frontend developer and vice-leader in the ICPC community. He excels in problem-solving and is progressing in Computer Science studies at the third level.</li>
+            <li class="team-member">Moustafa - Frontend Developer and ICPC Contributor: Moustafa, a frontend developer and active ICPC team member, demonstrates strong problem-solving skills. His dedication is evident as he progresses to the third level in Computer Science studies.</li>
+            <li class="team-member">Anas - Frontend Developer and ICPC Team Member: Anas, a dedicated frontend developer and ICPC team member, showcases strong problem-solving abilities. His commitment is apparent as he advances in Computer Science studies at the third </li>
+            <li class="team-member">Mohamed - AI Engineer and Computer Science Student: Mohamed specializes in Artificial Intelligence and excels in Computer Science studies, reaching the third level in his educational journey.</li>
+           
+        </ul>
+        
+        <p><strong>Our Collaborative Spirit:</strong> What sets us apart is our collaborative spirit. We believe in the power of teamwork, where each member's strengths complement the others. Together, we navigate challenges, celebrate victories, and constantly push the boundaries of what's possible.</p>
+        
+        <p><strong>Our Mission:</strong> The Web Development is on a mission to redefine success through innovation. We strive not only to meet expectations but to exceed them, leaving a lasting impact on every project we undertake.</p>
     </footer>
-    <!-- JavaScript File -->
-    <script src="script.js" defer></script>
-
-</body>
 
 </html>
